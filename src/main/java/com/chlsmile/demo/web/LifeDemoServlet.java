2e90f343bdf3516cfd29fd417820d945f74347a6
chlsmile <chl_smile@163.com>
2016-09-22 18:57:04 +0800

    add

---
+++ src/main/java/com/chlsmile/demo/web/LifeDemoServlet.java
@@ -0,0 +1,12 @@
+package com.chlsmile.demo.web;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created with: IntelliJ IDEA.
+ * Function:
+ * User: chl_smile
+ * Date: 2016-09-22 18:37:00
+ */
+public class LifeDemoServlet extends HttpServlet {
+}
