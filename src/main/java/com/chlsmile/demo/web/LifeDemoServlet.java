ef0d3ecc3bfcc7b7fe889b55d9e0805a0c247384
chlsmile <chl_smile@163.com>
2016-09-23 11:38:39 +0800

    add

--- src/main/java/com/chlsmile/demo/web/LifeDemoServlet.java
+++ src/main/java/com/chlsmile/demo/web/LifeDemoServlet.java
@@ -1,6 +1,10 @@
 package com.chlsmile.demo.web;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created with: IntelliJ IDEA.
@@ -9,4 +13,20 @@ import javax.servlet.http.HttpServlet;
  * Date: 2016-09-22 18:37:00
  */
 public class LifeDemoServlet extends HttpServlet {
+
+    public void init(ServletConfig config) throws ServletException{
+        System.out.println("LifeDemoServlet init 方法执行了");
+    }
+
+    public void destroy(){
+        System.out.println("LifeDemoServlet destroy 方法执行了");
+    }
+
+
+
+    public void service(HttpServletRequest req, HttpServletResponse resp){
+        System.out.println("LifeDemoServlet service 方法执行了");
+    }
+
+
 }
